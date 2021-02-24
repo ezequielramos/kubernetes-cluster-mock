0fadcf40daf93648be33ccab9e84a7816ede6086
Ezequiel Ramos <Ezequiel Ramos>
2021-02-24 11:56:46 -0300

    unified pod creation

--- app/resources/pods.py
+++ app/resources/pods.py
@@ -1,1 +1,83 @@
+import datetime
+import uuid
+
 items = {}
+
+
+def create_item(namespace, item):
+    global items
+
+    if namespace not in items:
+        items[namespace] = []
+
+    formated_item = {}
+
+    if "metadata" in item:
+        formated_item["metadata"] = item["metadata"]
+        formated_item["metadata"][
+            "selfLink"
+        ] = f'/api/v1/namespaces/{namespace}/pods/{item["metadata"]["name"]}'
+        formated_item["metadata"]["uid"] = str(uuid.uuid4())
+        formated_item["metadata"]["resourceVersion"] = "103524551"
+        formated_item["metadata"][
+            "creationTimestamp"
+        ] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    if "spec" in item:
+        formated_item["spec"] = item["spec"]
+
+    formated_item["status"] = {
+        "phase": "Running",
+        "conditions": [
+            {
+                "type": "Initialized",
+                "status": "True",
+                "lastProbeTime": None,
+                "lastTransitionTime": "2021-02-06T01:47:52Z",
+            },
+            {
+                "type": "Ready",
+                "status": "True",
+                "lastProbeTime": None,
+                "lastTransitionTime": "2021-02-08T14:45:05Z",
+            },
+            {
+                "type": "ContainersReady",
+                "status": "True",
+                "lastProbeTime": None,
+                "lastTransitionTime": "2021-02-08T14:45:05Z",
+            },
+            {
+                "type": "PodScheduled",
+                "status": "True",
+                "lastProbeTime": None,
+                "lastTransitionTime": "2021-02-06T01:47:51Z",
+            },
+        ],
+        "hostIP": "10.20.4.91",
+        "podIP": "10.20.4.154",
+        "startTime": "2021-02-06T01:47:52Z",
+        "containerStatuses": [
+            {
+                "name": "app",
+                "state": {"running": {"startedAt": "2021-02-08T14:44:39Z"}},
+                "lastState": {
+                    "terminated": {
+                        "exitCode": 137,
+                        "reason": "Error",
+                        "startedAt": "2021-02-06T22:26:28Z",
+                        "finishedAt": "2021-02-08T14:44:37Z",
+                        "containerID": "docker://1a2c158e0182bc4f9db9f75b61c680c8dee38cbf62d09c054a95cf314d004f20",
+                    }
+                },
+                "ready": True,
+                "restartCount": 2,
+                "image": "plataformproductionacr.azurecr.io/app:7513",
+                "imageID": "docker-pullable://plataformproductionacr.azurecr.io/app@sha256:c86f51fdc36e49378c762e965d8d3412f36657a89c71f1984ec34ccc53cf0ae2",
+                "containerID": "docker://fef3c93c47cf1d1c086f6350352dd915e9c083536f278b776da6efb11d518d32",
+            }
+        ],
+        "qosClass": "Burstable",
+    }
+
+    items[namespace].append(formated_item)
--- app/routes/v1/pods.py
+++ app/routes/v1/pods.py
@@ -5,7 +5,7 @@ import uuid
 
 from flask import Blueprint, request, Response
 
-from app.resources.pods import items
+from app.resources.pods import items, create_item
 
 logger = logging.getLogger(__name__)
 v1_pods = Blueprint("v1_pods", __name__)
@@ -13,84 +13,8 @@ v1_pods = Blueprint("v1_pods", __name__)
 
 @v1_pods.route("/api/v1/namespaces/<namespace>/pods", methods=["POST"])
 def post_pods(namespace):
-    new_ingress = json.loads(request.data)
-    if namespace not in items:
-        items[namespace] = []
-
-    formated_new_ingress = {}
-
-    if "metadata" in new_ingress:
-        formated_new_ingress["metadata"] = new_ingress["metadata"]
-        formated_new_ingress["metadata"][
-            "selfLink"
-        ] = f'/api/v1/namespaces/{namespace}/pods/{new_ingress["metadata"]["name"]}'
-        formated_new_ingress["metadata"]["uid"] = str(uuid.uuid4())
-        formated_new_ingress["metadata"]["resourceVersion"] = "103524551"
-        formated_new_ingress["metadata"][
-            "creationTimestamp"
-        ] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-
-    if "spec" in new_ingress:
-        formated_new_ingress["spec"] = new_ingress["spec"]
-
-    formated_new_ingress["status"] = {
-        "phase": "Running",
-        "conditions": [
-            {
-                "type": "Initialized",
-                "status": "True",
-                "lastProbeTime": None,
-                "lastTransitionTime": "2021-02-06T01:47:52Z",
-            },
-            {
-                "type": "Ready",
-                "status": "True",
-                "lastProbeTime": None,
-                "lastTransitionTime": "2021-02-08T14:45:05Z",
-            },
-            {
-                "type": "ContainersReady",
-                "status": "True",
-                "lastProbeTime": None,
-                "lastTransitionTime": "2021-02-08T14:45:05Z",
-            },
-            {
-                "type": "PodScheduled",
-                "status": "True",
-                "lastProbeTime": None,
-                "lastTransitionTime": "2021-02-06T01:47:51Z",
-            },
-        ],
-        "hostIP": "10.20.4.91",
-        "podIP": "10.20.4.154",
-        "startTime": "2021-02-06T01:47:52Z",
-        "containerStatuses": [
-            {
-                "name": "app",
-                "state": {"running": {"startedAt": "2021-02-08T14:44:39Z"}},
-                "lastState": {
-                    "terminated": {
-                        "exitCode": 137,
-                        "reason": "Error",
-                        "startedAt": "2021-02-06T22:26:28Z",
-                        "finishedAt": "2021-02-08T14:44:37Z",
-                        "containerID": "docker://1a2c158e0182bc4f9db9f75b61c680c8dee38cbf62d09c054a95cf314d004f20",
-                    }
-                },
-                "ready": True,
-                "restartCount": 2,
-                "image": "plataformproductionacr.azurecr.io/app:7513",
-                "imageID": "docker-pullable://plataformproductionacr.azurecr.io/app@sha256:c86f51fdc36e49378c762e965d8d3412f36657a89c71f1984ec34ccc53cf0ae2",
-                "containerID": "docker://fef3c93c47cf1d1c086f6350352dd915e9c083536f278b776da6efb11d518d32",
-            }
-        ],
-        "qosClass": "Burstable",
-    }
-
-    # if "status" in new_ingress:
-    #     formated_new_ingress["status"] = new_ingress["status"]
-
-    items[namespace].append(new_ingress)
+    pod = json.loads(request.data)
+    create_item(namespace, pod)
     return ""
 
 
